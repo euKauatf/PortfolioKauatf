034413974af7ce306932cbba81a93d5facf9beb8
EuKauatf <kauafvr1612@gmail.com>
2026-03-28 12:07:50 -0300

    fix(navbar): Arrumar rotas dos itens

--- app/src/components/navbar/navbar.tsx
+++ app/src/components/navbar/navbar.tsx
@@ -1,12 +1,14 @@
+import { NavLink } from "react-router-dom";
 import LeafIcon from "../../assets/images/leaf.svg?react";
+import "./navbar.css";
 
 function Navbar() {
   return (
     <div className="bg-special h-14 w-full shadow-md shadow-red-200">
-      <ul className="flex space-x-4 items-center h-full px-4 text-lg font-medium mx-4">
+      <ul className="flex space-x-4 geist-mono items-center h-full px-4 text-lg font-medium mx-4">
         <li className="flex justify-start">
           <a href="#">
-            <h1 className="flex items-center text-4xl color-special font-bold ml-1 geist-mono">
+            <h1 className="flex items-center text-4xl color-special font-bold">
               <span className="color-special-green">/</span>
               euKauatf
               <span className="ml-2">
@@ -16,14 +18,25 @@ function Navbar() {
           </a>
         </li>
         <li className="ml-auto">
-          <a href="/" className="text-white hover:text-gray-300">
+          <NavLink
+            to="/"
+            className={({ isActive }) =>
+              isActive ? "link liHover" : "liHover"
+            }
+          >
             Profissional
-          </a>
+          </NavLink>
         </li>
         <li className="ml-4">
-          <a href="/pessoal" className="text-white hover:text-gray-300">
+          <NavLink
+            to="/pessoal"
+            end
+            className={({ isActive }) =>
+              isActive ? "link liHover" : "liHover"
+            }
+          >
             Pessoal
-          </a>
+          </NavLink>
         </li>
       </ul>
     </div>
