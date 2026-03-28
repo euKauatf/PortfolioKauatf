598f0a94d358179e0279e195e11694a67f62177e
EuKauatf <kauafvr1612@gmail.com>
2026-03-28 11:11:07 -0300

    feat(navbar): Estilizar e adicionar itens

--- app/src/components/navbar/navbar.tsx
+++ app/src/components/navbar/navbar.tsx
@@ -1,9 +1,31 @@
+import LeafIcon from "../../assets/images/leaf.svg?react";
+
 function Navbar() {
   return (
     <div className="bg-special h-14 w-full shadow-md shadow-red-200">
-      <h1 className="text-6xl color-special font-bold ml-1 geist-mono">
-        euKauatf
-      </h1>
+      <ul className="flex space-x-4 items-center h-full px-4 text-lg font-medium mx-4">
+        <li className="flex justify-start">
+          <a href="#">
+            <h1 className="flex items-center text-4xl color-special font-bold ml-1 geist-mono">
+              <span className="color-special-green">/</span>
+              euKauatf
+              <span className="ml-2">
+                <LeafIcon className="w-8 h-8 fill-[#4F772D]" />
+              </span>
+            </h1>
+          </a>
+        </li>
+        <li className="ml-auto">
+          <a href="/" className="text-white hover:text-gray-300">
+            Profissional
+          </a>
+        </li>
+        <li className="ml-4">
+          <a href="/pessoal" className="text-white hover:text-gray-300">
+            Pessoal
+          </a>
+        </li>
+      </ul>
     </div>
   );
 }
